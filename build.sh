573df5dc111d97cf43f985680c800460a008ef9e
Radovan Synek <rsynek@redhat.com>
2019-03-05 12:08:02 +0100

    Replace S2i with fabric8 maven plugin

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-mvn clean package && docker build \
-  --build-arg JAR_PATH=target/optaplanner-demo.jar \
-  --tag optaplanner-demo \
-  "."
--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-IMAGE=optaplanner-demo:latest
-CONTAINER_NAME=optaplanner-demo
-
-docker kill $CONTAINER_NAME > .optaplanner-demo.log 2>&1
-docker rm $CONTAINER_NAME > .optaplanner-demo.log 2>&1
-docker run -d -p 8080:8080 --name $CONTAINER_NAME $IMAGE
