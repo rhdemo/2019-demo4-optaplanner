5344ce62a28d2704dfd34a1fe7fb4034a0e0efd2
Radovan Synek <rsynek@redhat.com>
2019-04-23 22:53:09 +0200

    Cover more cases for reconnection to Infinispan

--- src/main/java/com/redhat/demo/optaplanner/upstream/InfinispanConnector.java
+++ src/main/java/com/redhat/demo/optaplanner/upstream/InfinispanConnector.java
@@ -1,18 +1,8 @@
 package com.redhat.demo.optaplanner.upstream;
 
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.ExecutionException;
-
-import javax.annotation.PostConstruct;
-
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.redhat.demo.optaplanner.Mechanic;
-import com.redhat.demo.optaplanner.SpringProfiles;
-import com.redhat.demo.optaplanner.config.AppConfiguration;
 import com.redhat.demo.optaplanner.upstream.utils.GameConfigListener;
 import com.redhat.demo.optaplanner.upstream.utils.OptaPlannerConfig;
 import com.redhat.demo.optaplanner.websocket.response.FutureVisitsResponse;
@@ -25,36 +15,34 @@ import org.infinispan.client.hotrod.RemoteCounterManagerFactory;
 import org.infinispan.client.hotrod.configuration.Configuration;
 import org.infinispan.counter.api.CounterManager;
 import org.infinispan.counter.api.StrongCounter;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Service;
 
-@Service
-@Profile(SpringProfiles.PRODUCTION)
-public class InfinispanConnector implements UpstreamConnector {
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.RejectedExecutionException;
+
+
+class InfinispanConnector implements UpstreamConnector {
 
     private static final long FULL_HEALTH = 1_000_000_000_000_000_000L;
     private static final String DISPATCH_MECHANIC_EVENTS_CACHE_NAME = "DispatchEvents";
     private static final String DEFAULT_CACHE_NAME = "default";
     public static final String OPTA_PLANNER_CONFIG_KEY_NAME = "OptaPlannerConfig";
 
-    @Autowired
-    private AppConfiguration appConfiguration;
-    @Autowired
-    GameConfigListener gameConfigListener;
-
     private StrongCounter[] counters;
     private Map<StrongCounter, Integer> counterIndices;
     private RemoteCache<String, String> dispatchMechanicEventsCache;
     private RemoteCache<String, String> defaultCache;
+    private RemoteCacheManager remoteCacheManager;
     private ObjectMapper objectMapper;
 
-    @PostConstruct
-    public void postConstruct() {
-        counters = new StrongCounter[appConfiguration.getMachinesOnlyLength()];
+    protected InfinispanConnector(int machineHealthCountersCount, GameConfigListener gameConfigListener) {
+        counters = new StrongCounter[machineHealthCountersCount];
         counterIndices = new HashMap<>(counters.length);
         Configuration configuration = HotRodClientConfiguration.get().build();
-        RemoteCacheManager remoteCacheManager = new RemoteCacheManager(configuration);
+        remoteCacheManager = new RemoteCacheManager(configuration);
         CounterManager counterManager = RemoteCounterManagerFactory.asCounterManager(remoteCacheManager);
         for (int i = 0; i < counters.length; i++) {
             StrongCounter currentCounter = counterManager.getStrongCounter(String.format("machine-%d", i));
@@ -70,6 +58,10 @@ public class InfinispanConnector implements UpstreamConnector {
         defaultCache.put(OPTA_PLANNER_CONFIG_KEY_NAME, convertToJsonString(defaultConfig));
     }
 
+    public void disconnect() {
+        remoteCacheManager.stop();
+    }
+
     @Override
     public double[] fetchMachineHealths() {
         return Arrays.stream(counters)
@@ -81,7 +73,7 @@ public class InfinispanConnector implements UpstreamConnector {
                         Thread.currentThread().interrupt();
                         throw new IllegalStateException("Connector thread was interrupted while getting counter value.", e);
                     } catch (ExecutionException e) {
-                        throw new IllegalStateException("Couldn't find StringCounter (" + counterIndex + ").", e.getCause());
+                        throw new InfinispanException("Couldn't find StringCounter (" + counterIndex + ").", e.getCause());
                     }
                 })
                 .mapToDouble(machineHealthLong -> ((double) machineHealthLong) / ((double) FULL_HEALTH))
@@ -90,7 +82,11 @@ public class InfinispanConnector implements UpstreamConnector {
 
     @Override
     public void resetMachineHealth(int machineIndex) {
-        counters[machineIndex].reset();
+        try {
+            counters[machineIndex].reset();
+        } catch (RejectedExecutionException ex) {
+            throw new InfinispanException("Unable to reset health of the machine-" + machineIndex + ".", ex);
+        }
     }
 
     @Override
@@ -116,7 +112,11 @@ public class InfinispanConnector implements UpstreamConnector {
     @Override
     public void damageMachine(int machineIndex, double damage) {
         long damageLong = (long) (damage * FULL_HEALTH);
-        counters[machineIndex].addAndGet(-damageLong);
+        try {
+            counters[machineIndex].addAndGet(-damageLong);
+        } catch (RejectedExecutionException ex) {
+            throw new InfinispanException("Unable to damage the machine-" + machineIndex + ".", ex);
+        }
     }
 
     @Override
@@ -127,16 +127,24 @@ public class InfinispanConnector implements UpstreamConnector {
 
     @Override
     public synchronized void setDispatchStatus(boolean isDispatchActive) {
-        OptaPlannerConfig config = convertFromJsonString(defaultCache.get(OPTA_PLANNER_CONFIG_KEY_NAME), OptaPlannerConfig.class);
+        OptaPlannerConfig config = getOptaPlannerConfig();
         config.setDispatchActive(isDispatchActive);
         defaultCache.put(OPTA_PLANNER_CONFIG_KEY_NAME, convertToJsonString(config));
     }
 
     @Override
     public synchronized void setSimulationStatus(boolean isSimulationActive) {
-        OptaPlannerConfig config = convertFromJsonString(defaultCache.get(OPTA_PLANNER_CONFIG_KEY_NAME), OptaPlannerConfig.class);
+        OptaPlannerConfig config = getOptaPlannerConfig();
         config.setSimulationActive(isSimulationActive);
         defaultCache.put(OPTA_PLANNER_CONFIG_KEY_NAME, convertToJsonString(config));
+    }
+
+    private OptaPlannerConfig getOptaPlannerConfig() {
+        String jsonString = defaultCache.get(OPTA_PLANNER_CONFIG_KEY_NAME);
+        if (jsonString == null) {
+            throw new InfinispanException(OPTA_PLANNER_CONFIG_KEY_NAME + " was null. Try to reconnect.");
+        }
+        return convertFromJsonString(jsonString, OptaPlannerConfig.class);
     }
 
     private String convertToJsonString(Object object) {
